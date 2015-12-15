bfb0936752a457a2d7dafef67543bd84a51ef5f2
Klemens Schölhorn <klemens@schoelhorn.eu>
2015-12-15 22:21:43 +0100

    Update script-extractor docs

--- docs/implementors/core/convert/trait.Into.js
+++ docs/implementors/core/convert/trait.Into.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['script_extractor'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.Into.html' title='core::convert::Into'>Into</a>&lt;&amp;'static <a href='https://doc.rust-lang.org/nightly/std/primitive.str.html'>str</a>&gt; for <a class='enum' href='script_extractor/parse/enum.LocationType.html' title='script_extractor::parse::LocationType'>LocationType</a>",];
+implementors['script_extractor'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.Into.html' title='core::convert::Into'>Into</a>&lt;&amp;'static <a href='https://doc.rust-lang.org/nightly/std/primitive.str.html'>str</a>&gt; for <a class='enum' href='script_extractor/enum.LocationType.html' title='script_extractor::LocationType'>LocationType</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- docs/implementors/core/default/trait.Default.js
+++ docs/implementors/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['script_extractor'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='script_extractor/parse/struct.Location.html' title='script_extractor::parse::Location'>Location</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='enum' href='script_extractor/parse/enum.LocationType.html' title='script_extractor::parse::LocationType'>LocationType</a>",];
+implementors['script_extractor'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='script_extractor/struct.Location.html' title='script_extractor::Location'>Location</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='enum' href='script_extractor/enum.LocationType.html' title='script_extractor::LocationType'>LocationType</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
